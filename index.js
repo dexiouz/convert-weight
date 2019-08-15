1d8e0fffaf7ba8ac29c3fd7ea7d9d840921fb956
Teezolusegun <teezolusegun@gmail.com>
2019-08-15 12:59:38 +0100

    changed the event listener to a stand alone function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // document.querySelector('#gramsOutPut').style.visibility = 'hidden'
 // document.querySelector('#kgOutPut').style.visibility = 'hidden'
 // document.querySelector('#ounceOutPut').style.visibility = 'hidden'
-document.querySelector("#input").addEventListener("input", function(e) {
+document.querySelector("#input").addEventListener("input", convert)
+
+function convert (e) {
   document.querySelector("#gramsOutPut").style.visibility = "visible";
   document.querySelector("#kgOutPut").style.visibility = "visible";
   document.querySelector("#ounceOutPut").style.visibility = "visible";
@@ -17,4 +19,4 @@ document.querySelector("#input").addEventListener("input", function(e) {
     pounds / 2.2046);
   const ounces = (document.querySelector("#ounceOutPut span").innerHTML =
     pounds * 16);
-});
+};
